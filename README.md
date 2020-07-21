26d6cbb23944e9c0efa9454f40bc934a274cde83
-- <-->
2020-07-21 08:49:16 -0400

    run-all && wait-on

--- README.md
+++ README.md
@@ -25,3 +25,11 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+## Helpful Links
+
+https://stackoverflow.com/questions/54205211/how-do-i-prevent-powershell-from-failing-if-karma-has-failed-tests
+
+https://medium.com/angular-in-depth/the-angular-devops-series-ct-and-code-coverage-with-teamcity-21e3d0ed77eb
+
+https://medium.com/@mgd4375/building-an-angular-cli-project-in-teamcity-714a894f7083
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,15 +9,21 @@ describe('workspace-project App', () => {
   });
 
   it('should display welcome message', () => {
+    expect(browser.params.login.password).toEqual(123456);
+
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('cli-protractor-sandbox app is running!');
+    expect(page.getTitleText()).toEqual(
+      'cli-protractor-sandbox app is running!'
+    );
   });
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
+    expect(logs).not.toContain(
+      jasmine.objectContaining({
+        level: logging.Level.SEVERE,
+      } as logging.Entry)
+    );
   });
 });
